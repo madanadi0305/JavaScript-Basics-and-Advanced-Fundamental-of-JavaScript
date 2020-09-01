8429ae27e378bec2b80d9bbccba1b5483eb547d6
madanadi0305 <madanadi0305@gmail.com>
2020-09-01 16:18:32 +0000

    Update task06.js

--- task06.js
+++ task06.js
@@ -2,7 +2,9 @@ function task06(){
     // Use variable x and y for storing data
     // Use variable result for storing sum of previous variable
     // Enter your logic here
-
+   var a=10;
+   var b=20;
+   sum=a+b;
     return sum
 }
 
