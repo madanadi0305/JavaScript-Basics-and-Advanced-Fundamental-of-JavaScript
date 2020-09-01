932e0a45ab1a644ea3207b74c21744293f1670b3
madanadi0305 <madanadi0305@gmail.com>
2020-09-01 16:54:33 +0000

    Update task07.js

--- task07.js
+++ task07.js
@@ -2,8 +2,10 @@ function task07(){
     // Use two variables first and second to store your data
     // After multiplication store it's output in result vaiable
     // Enter your logic here
-
-
+   var num1=2;
+   var num2=10;
+   result=num1*num2; 
+   console.log(result);
     return result
 }
 
