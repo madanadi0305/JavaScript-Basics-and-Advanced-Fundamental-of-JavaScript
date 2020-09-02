4f6ce5cec95d92d4f8396f41cf8a4a875bf56ddf
madanadi0305 <madanadi0305@gmail.com>
2020-09-02 06:31:08 +0000

    Update task13.js

--- task13.js
+++ task13.js
@@ -2,15 +2,17 @@ function task13(year){
     var yes_or_no=0;
     if(year%4==0 && year%100==0){
      yes_or_no=1;
-     return yes_or_no;
+     console.log(yes_or_no);
+     //return yes_or_no;
     }
     else if(year%400==0 && year%100==0){
         yes_or_no=1;
-        return yes_or_no;
+        console.log(yes_or_no);
     }
 else{
     console.log(yes_or_no);
-    return yes_or_no;
+    
+    //return yes_or_no;
 }
 //return yes_or_no;
 }
