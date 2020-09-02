17cce365f5ce6041d4bd9da7e1476a6b81bf45ea
madanadi0305 <madanadi0305@gmail.com>
2020-09-02 06:53:44 +0000

    Update task13.js

--- task13.js
+++ task13.js
@@ -1,6 +1,6 @@
 function task13(year){
     let yes_or_no=1;
-    if(year%4===0 || year%100===0){
+    if(year%4===0 && year%100===0){
      //yes_or_no=1;
      //console.log(yes_or_no);
      //return yes_or_no;
