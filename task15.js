1cd119d7da343f1cb3083e22cd8bc192b86756b6
madanadi0305 <madanadi0305@gmail.com>
2020-09-02 07:42:55 +0000

    Update wiki/task15.md, task15.js files

---
+++ task15.js
@@ -0,0 +1,13 @@
+function task15(number1,number2){
+    var sum=0;
+  //  var sum1=0;
+    if(num1==num2){
+     sum=num1+num2;
+     sum=sum*3;
+    }
+    else{
+        sum=num1+num2;
+    }
+    return sum;
+}
+module.exports=task15;
