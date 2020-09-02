35c0f15e276f0ce981011c65d9665bba7b2126d9
madanadi0305 <madanadi0305@gmail.com>
2020-09-02 04:23:49 +0000

    Update task08.js

--- task08.js
+++ task08.js
@@ -1,9 +1,11 @@
-function task08(num1,num2){
-
+function task08(){
+   
     // Enter your logic here
+   var num1=200;
+   var num2=300;
     var sum=num1+num2;
     return sum;
 }
 
-//module.exports = task08
-task08(200,300)
+module.exports = task08
+//task08(200,300)
