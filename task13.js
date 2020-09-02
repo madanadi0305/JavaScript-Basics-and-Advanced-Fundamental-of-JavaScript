5de57fc92907f3b70ccbf9ff37681fe255140dde
madanadi0305 <madanadi0305@gmail.com>
2020-09-02 06:49:05 +0000

    Update task13.js

--- task13.js
+++ task13.js
@@ -1,19 +1,21 @@
 function task13(year){
-    var yes_or_no=0;
-    if(year%4==0 && year%100==0){
+    let yes_or_no=0;
+    if(year%4===0 || year%100===0){
      yes_or_no=1;
-     console.log(yes_or_no);
+     //console.log(yes_or_no);
      //return yes_or_no;
     }
-    else if(year%400==0 && year%100==0){
+    else if(year%400===0 && year%100===0){
         yes_or_no=1;
-        console.log(yes_or_no);
+       // console.log(yes_or_no);
     }
 else{
-    console.log(yes_or_no);
+  yes_or_no=0;
+   // console.log(yes_or_no);
     
     //return yes_or_no;
 }
-//return yes_or_no;
+return yes_or_no;
 }
+//document.getElementById('para1').innerHTML=task13(1982);
 module.exports=task13;
