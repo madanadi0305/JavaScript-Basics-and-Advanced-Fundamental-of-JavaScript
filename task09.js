d3c2adb95c3571c4b698cd37b7f1d10cd59bc18a
madanadi0305 <madanadi0305@gmail.com>
2020-09-02 04:31:18 +0000

    Update task09.js

---
+++ task09.js
@@ -0,0 +1,11 @@
+function task09(){
+    var subject1=50;
+    var subject2=40;
+    var subject3=60;
+    var subject4=90;
+    var subject5=10;
+    var sum=subject1+subject2+subject3+subject4+subject5;
+    var percent=(sum/500)*100;
+    console.log(percent);
+}
+module.exports=task09;
