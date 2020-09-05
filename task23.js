cfa932cdf32e31a204fb164f12ea87d898a91277
madanadi0305 <madanadi0305@gmail.com>
2020-09-05 07:53:09 +0000

    Update task23.js

---
+++ task23.js
@@ -0,0 +1,12 @@
+function task23(num1,num2){
+    var sum=num1+num2;
+    var default_value=65;
+    if(sum>=50 && sum<=80){
+        default_value=65;
+    }
+    else{
+        default_value=80;
+    }
+    return defaut_value;
+}
+module.exports=task23;
