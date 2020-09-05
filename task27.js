bf4c4d35c556df592fa8281f4b7c97bc1d5091ee
madanadi0305 <madanadi0305@gmail.com>
2020-09-05 17:07:54 +0000

    Update task27.js

--- task27.js
+++ task27.js
@@ -1,6 +1,6 @@
 function task27(num1,num2,num3){
     var result=40;
-    if(num1===num2===num3){
+    if((num1===num2)&&(num1===num3)&&(num1===num3)){
         result=30;
     }
     else if((num1===num2) || (num2===num3) || (num1===num3)){
