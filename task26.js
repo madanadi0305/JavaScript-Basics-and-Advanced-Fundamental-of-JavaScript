3c9d38756818ed6b771a3313aaa035759ac04901
madanadi0305 <madanadi0305@gmail.com>
2020-09-05 16:13:49 +0000

    Update task26.js

--- task26.js
+++ task26.js
@@ -1,7 +1,7 @@
 function task26(num1,num2){
    var sum=num1+num2;
    var result=false;
-    if(num1===8 && num2===8){
+    if((num1===8) && (num2===8)){
         result=true;
     }
     else if(sum===8){
