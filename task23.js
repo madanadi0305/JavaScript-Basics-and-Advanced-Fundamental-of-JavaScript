4d2f4adf761da06a125ecb205c5b7bad4a23d7a7
madanadi0305 <madanadi0305@gmail.com>
2020-09-05 08:26:50 +0000

    Update task23.js

--- task23.js
+++ task23.js
@@ -1,12 +1,12 @@
 function task23(num1,num2){
     var sum=num1+num2;
-    var default_value=80;
+    var default_val=80;
     if(sum>=50 && sum<=80){
-        default_value=65;
+        default_val=65;
     }
     else{
-        default_value=80;
+        default_val=80;
     }
-    return defaut_value;
+    return defaut_val;
 }
 module.exports=task23;
