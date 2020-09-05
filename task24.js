cbf4d6deb25989d09b1d92281b89ed10585a232f
madanadi0305 <madanadi0305@gmail.com>
2020-09-05 09:24:49 +0000

    Update task24.js

---
+++ task24.js
@@ -0,0 +1,21 @@
+function task24(){
+    var direction=null;
+    var directon_integer=1;
+    switch(direction_integer){
+      case 1:
+      direction='East';
+      break;
+      case 2:
+      direction='West';
+      break;
+      case 3:
+      direction='North';
+      break;
+      case 4:
+      direction='South';
+      break;
+      default:direction='invalid';
+    }
+    return direction;
+}
+module.exports=task24;
